df87daa2eef4d7fd8e64d864f30423c3d51c36f3
johnngugi <johnngugi777@gmail.com>
2018-09-24 11:24:44 +0300

    Add new validation

---
+++ js/form_validation.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    console.log("hello");
+
+    var pwd = $("#loginform #password");
+    $("#loginform #password").focusout(function() {
+        console.log(pwd.val());
+        if (pwd.val() ==null || pwd.val()=="") {
+            $("#pwdalert").show();
+        }
+    });
+
+    function validatelogin(){
+        var user = document.loginform.username.value;
+        var pwd = document.loginform.pwd.value;
+        
+    }
+
+});
