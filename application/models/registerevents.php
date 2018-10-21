9f68edc858069db7127d3010b815993d336dbb87
Wanjiru Kagema <maureenkagema@gmail.com>
2018-10-21 10:55:37 +0300

    events and register an event

--- application/models/registerevents.php
+++ application/models/registerevents.php
@@ -3,7 +3,7 @@ class Registerevents extends CI_Model
 {
 	function save ($Event_no,$Event_name,$Description)
 	{
-	$query="insert into events values('$Event_no','$Event_name','$Description')";
+	$query="insert into events(Event_no, Event_name, Description) values('$Event_no','$Event_name','$Description')";
 	$this->db->query($query);
 	}
 }
