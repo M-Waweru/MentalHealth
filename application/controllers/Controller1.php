bc3aba91c12b6f4a3e92143a82652ec7fe26e10c
EssieNjoroge <njoroge.esther@strathmore.edu>
2018-10-22 12:15:28 +0300

    Empty

---
+++ application/controllers/Controller1.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Controller1 extends CI_Controller {
+
+	
+	public function index()
+	{
+		$this->load->view('blog');
+		
+		
+$this->load->helper('url');
+
+		
+	}
+
+	public function displayUpload(){
+		$this->load->view('blog');
+	
+	}	
+	
+}
